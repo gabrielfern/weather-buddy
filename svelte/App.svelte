d52e1eba96fd804717e624862a3752f8c984a865
Gabriel Fernandes <gabrielfernnd@gmail.com>
2021-02-28 23:32:45 -0300

    Better design and another svelte component

--- svelte/App.svelte
+++ svelte/App.svelte
@@ -1,39 +1,97 @@
 <script>
+    import City from './City.svelte'
+    import { onMount, afterUpdate } from 'svelte'
+
     let cities = []
+    let city_name = ''
+    let input_disabled = false
+    let error_msg = ''
+    let city
+    let input
+
     async function load_cities() {
         let resp = await fetch('API_BASE_URL/api/weather')
         cities = await resp.json()
     }
-    load_cities()
 
-    let city_name = ''
-    let input_disabled = false
     async function retrieve_city() {
         input_disabled = true
-        await fetch(`API_BASE_URL/api/weather/${city_name}`)
-        await load_cities()
-        city_name = ''
+        error_msg = ''
+
+        try {
+            let resp = await fetch(`API_BASE_URL/api/weather/${city_name}`)
+            if (resp.status == 200) {
+                city = await resp.json()
+                city_name = ''
+                load_cities()
+            } else if (resp.status == 404) {
+                error_msg = 'Sorry. We couldn\'t find the specified city.'
+            } else {
+                throw Error()
+            }
+        } catch {
+            error_msg = 'An error has occurred.'
+        }
+
         input_disabled = false
     }
+
+    onMount(load_cities)
+    afterUpdate(() => input && input.focus())
 </script>
 
 <div class='container'>
     <h1>Weather Buddy</h1>
 
     <form on:submit|preventDefault={retrieve_city}>
-        <input bind:value={city_name} type='text' disabled={input_disabled}>
-        <button type='submit' disabled={input_disabled}>Go</button>
+        How is the weather in
+        <input bind:value={city_name} type='text' disabled={input_disabled} bind:this={input}>
+        now?
     </form>
 
-    {#each cities as city}
-        <h4>{city.name} ({city.temperature} °C)</h4>
-    {/each}
+    {#if error_msg}
+        <h3 class='error'>{error_msg}</h3>
+    {/if}
+
+    {#if city}
+        <div class='inner-container'>
+            <City city={city} spotlight={true} />
+        </div>
+    {/if}
+
+    <div class='inner-container'>
+        {#each cities as city}
+            <City city={city} spotlight={false} />
+        {/each}
+    </div>
 </div>
 
 <style>
+    h1, form {
+        margin-bottom: 30px;
+    }
+
+    input {
+        border-top: none;
+        border-left: none;
+        border-right: none;
+    }
+
     .container {
         display: flex;
         flex-direction: column;
         align-items: center;
+    }
+
+    .inner-container {
+        margin-top: 50px;
+        margin-bottom: 50px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+
+    .error {
+        color: red;
     }
 </style>
---
+++ svelte/City.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let city
+    export let spotlight
+</script>
+
+<div class:shadow={spotlight}>
+    <p>{city.name}</p>
+    <p class='temperature'>{city.temperature} °C</p>
+    <p>{city.weather}</p>
+</div>
+
+<style>
+    div {
+        padding: 10px;
+        margin: 10px;
+        width: 150px;
+        text-align: center;
+        border: 1px solid black;
+    }
+
+    .temperature {
+        font-size: xx-large;
+    }
+
+    .shadow {
+        box-shadow: 5px 5px;
+    }
+</style>
